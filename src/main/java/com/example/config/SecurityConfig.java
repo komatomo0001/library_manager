3ae0cc8bf5b8fae1efb841db5ca8ebccb408aebf
komatsu tomoki <ponjujujujujujuju@gmail.com>
2023-09-28 18:05:15 +0900

    lesson3 finish

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
     protected void configure(HttpSecurity http) throws Exception{
 		
-		
 		http.authorizeRequests()
         .antMatchers("/loginForm").permitAll()
         .anyRequest().authenticated();
